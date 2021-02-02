d1a8bb69de15530d8eca102185192878f2f5ef9b
guille Craven <guillecraven@gmail.com>
2021-02-02 13:56:09 -0300

    primer commit

---
+++ js/ejercicio4.js
@@ -0,0 +1,1 @@
+prompt("Nombre de usuario: ")
---
+++ js/ejercicio6.js
@@ -0,0 +1,3 @@
+let numero1 = parseInt(prompt("primer numero: "))
+let numero2 = parseInt(prompt("segundo numero: "))
+if (numero1 > numero2) { document.write(numero1) } else { document.write(numero2) }
